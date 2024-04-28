f9b102434a816c7835c3a8c9c160cb5f98985796
coradini1 <gabe.cora1@hotmail.com>
2024-04-28 21:26:12 +0100

    refactor

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,8 +1,4 @@
 import styles from "./styles.module.css";
-import { Input } from "@/components/ui/input";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
 export default function Home() {
   return (
@@ -15,14 +11,7 @@ export default function Home() {
 function LoginModal() {
   return (
     <div className={styles.modalCard}>
-      <h1>Acesse sua conta</h1>
-      <input placeholder="Email" />
-      <input placeholder="Senha" />
-      <p>
-        Esqueceu sua senha?{" "}
-        <a href="#" style={{ color: "rgb(114, 135, 253)" }}>redefinir senha</a>
-      </p>
-    <button>Log in</button>
+      
     </div>
   );
 }
