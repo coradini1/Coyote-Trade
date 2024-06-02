7ee9db555458cb9741f6b3e07908c5ef8094ab5a
coradini1 <gabe.cora1@hotmail.com>
2024-06-02 10:02:13 -0300

    fix: typos

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -2,11 +2,7 @@ import { Request, Response } from "express"
 
 import { db } from "../db/db"
 
-<<<<<<<< HEAD:backend/src/controllers/userController.ts
 export async function userController(req: any, res: Response) {
-========
-export async function meController(req: any, res: Response) {
->>>>>>>> 47086b14a1c7ace25fd7123779f59ea43d692a88:backend/src/controllers/meController.ts
   const userData = await db.query.usersTable.findFirst({
     where: (user, { eq }) => eq(user.email, req.user.email),
   })
