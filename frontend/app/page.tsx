16b3e86b45bee024d9c3d347a602c62bbe83829c
coradini1 <gabe.cora1@hotmail.com>
2024-06-16 22:11:43 -0300

    fix: app index

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,13 @@
+"use client";
 import Link from "next/link";
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    window.location.href = "/login";
+  }, []);
   return (
-    <main>
-      <h1>index</h1>
+    <main style={{ display: "none" }}>
       <Link href="/login">login</Link>
       <Link href="/register">register</Link>
     </main>
