42ab6538530ba7d9737afe53b44e58ffc7576bb4
coradini1 <gabe.cora1@hotmail.com>
2024-06-16 22:15:43 -0300

    feat: add dashboard page

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,10 +1,63 @@
 "use client";
-import React from "react";
+import React, {useState, useEffect  } from "react";
 import UserCard from "@/components/custom/UserCard/UserCard";
 import Orders from "@/components/custom/Orders/Orders";
 import Portfolio from "@/components/custom/Portfolio/Portfolio";
 import Investments from "@/components/custom/Investments/Investments";
+
 export default function Page() {
+
+  const [user, setUser] = useState<any>();
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/user`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        }
+      );
+
+      const userData = await response.json();
+
+      if (userData?.user?.role !== "admin" && userData?.user?.role !== "user" || !userData) {
+        return (window.location.href = "/login");
+      }
+
+      const metrics = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/metrics/trends`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        }
+      );
+
+      const userMetrics = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/metrics/users?page=1&limit=15`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        }
+      );
+
+
+      setUser(userData.user);
+    }
+
+    fetchData();
+  }, []);
+
+
   return (
     <div>
       <div className="p-4">
