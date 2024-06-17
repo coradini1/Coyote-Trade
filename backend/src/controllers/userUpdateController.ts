ecaf05fb56a4e4f558cc22b138f3d3e7e813f15b
coradini1 <gabe.cora1@hotmail.com>
2024-06-16 22:05:47 -0300

    feat: add update user

--- backend/src/controllers/userUpdateController.ts
+++ backend/src/controllers/userUpdateController.ts
@@ -5,10 +5,14 @@ import { usersTable } from "../db/schema"
 import { db } from "../db/db"
 
 export async function userUpdateController(req: any, res: Response) {
-  const { role, email } = req.body.user
+  const { role, email, address, name, surname } = req.body.user
 
   try {
-    await db.update(usersTable).set({ role: role }).where(eq(usersTable.email, email))
+    if(name) await db.update(usersTable).set({ name: name }).where(eq(usersTable.email, email))
+    if(surname) await db.update(usersTable).set({ surname: surname }).where(eq(usersTable.email, email))
+    if(address) await db.update(usersTable).set({ address: address }).where(eq(usersTable.email, email))
+    if(role) await db.update(usersTable).set({ role: role }).where(eq(usersTable.email, email))
+    
   } catch (e) {
     res.status(500).json({ message: "Error updating user" })
   }  
