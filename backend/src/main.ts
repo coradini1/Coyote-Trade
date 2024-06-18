8c118f94cf8ea0b0c4512c1430a2131847690efe
coradini1 <gabe.cora1@hotmail.com>
2024-06-17 23:08:59 -0300

    feat: WIP stripe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,22 +4,42 @@ import cors from "cors"
 import bodyParser from "body-parser"
 import cookieParser from "cookie-parser"
 
+import Stripe from "stripe"
+
 import login from "./routes/login"
 import register from "./routes/register"
 import user from "./routes/user"
 import metrics from "./routes/metrics"
+
+
 // Server
 const app: Express = express()
 const port = 3002
 
+
+
+
 // Middlewares
 app.use(cors({
   credentials: true,
-  origin: "https://coyotetrade.com.br"
+  origin: "http://localhost:3000"
 }))
 app.use(cookieParser())
 app.use(bodyParser.json())
 
+
+// Stripe
+const stripe_secret = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
+  apiVersion: '2024-04-10',
+})
+app.get("/api/stripe", async (req, res) => {
+  try {
+    res.send({
+      publicKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    });
+  } catch (err) {
+  }
+});
 // Routes
 app.use("/api/register", register)
 app.use("/api/login", login)
--- frontend/components/custom/UserCard/UserCard.tsx
+++ frontend/components/custom/UserCard/UserCard.tsx
@@ -1,21 +1,30 @@
 "use client";
-import React, {use, useEffect, useState} from "react";
+import React, { use, useEffect, useState } from "react";
 
-
-function UserCard({userData }: any) {
-
+function UserCard({ userData }: any) {
   useEffect(() => {
+  }, [userData]);
+
+  const fetchStripePublic = async () => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/stripe`);
+    const data = await response.json();
+    console.log(data)
+    
   }
-  , [userData]);
 
   return (
     <div className="user-card bg-white p-4 rounded shadow">
-      <h2 className="text-lg font-bold">{userData?.name} {userData?.surname}</h2>
+      <h2 className="text-lg font-bold">
+        {userData?.name} {userData?.surname}
+      </h2>
       <p>Account settings</p>
       <p>Balance: ${userData?.balance}</p>
       <p>Cash Available: ${userData?.balance}</p>
       <div className="flex space-x-2 mt-4">
-        <button className="bg-blue-500 text-white px-4 py-2 rounded">
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+          onClick={fetchStripePublic}
+        >
           Deposit
         </button>
         <button className="bg-red-500 text-white px-4 py-2 rounded">
@@ -40,6 +49,6 @@ function UserCard({userData }: any) {
       </div>
     </div>
   );
-};
+}
 
 export default UserCard;
