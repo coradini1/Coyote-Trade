f661dd7afaeada74adb406c46fbe6b0385c9af99
Coradini <88799439+coradini1@users.noreply.github.com>
2025-04-29 22:24:05 -0300

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -23,7 +23,7 @@ const port = 3002;
 app.use(
   cors({
     credentials: true,
-    origin: "https://coyote-trade.vercel.app",
+    origin: "https://coyotetrade.com.br",
   })
 );
 app.use(cookieParser());
